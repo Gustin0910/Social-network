8ee4a6a92aa6b8f6d5099a95a67183f85d673c5c
Gustin <dobrovolskiy0910@gmail.com>
2026-05-07 01:47:16 +0300

    Add files via upload

---
+++ posts/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница проекта Yatube')
+
+def group_posts(request,slug):
+    return HttpResponse(f'пост:{slug}')
